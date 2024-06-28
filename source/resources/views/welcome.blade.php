f6c1dca75bcd17c4bd0eb36a89253828e2081971
Majinbuu <majinbuuu113@gmail.com>
2024-06-28 23:26:32 +0700

    update

--- source/resources/views/welcome.blade.php
+++ source/resources/views/welcome.blade.php
@@ -40,10 +40,6 @@
                 <a href="/"><img class="main" src="{{ asset('images/button-chinh-dien.png') }}" alt=""></a>
                 <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-chinh-dien-active.png') }}" alt=""></a>
             </li>
-{{--            <li class="item_menu_game">--}}
-{{--                <a href=""><img src="{{ asset('images/button-luan-ban.png') }}" alt=""></a>--}}
-{{--                <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-luan-ban-active.png') }}" alt=""></a>--}}
-{{--            </li>--}}
             @if(session()->get('user'))
                 <li class="item_menu_game">
                     <a href="/tai-khoan"><img src="{{ asset('images/button-tai-khoan.png') }}" alt=""></a>
@@ -53,6 +49,10 @@
                     <a href="/dang-xuat" title="Đăng nhập"><img src="{{ asset('/images/button-dang-xuat.png') }}" alt="Đăng xuất" title="Đăng xuất"></a>
                 </li>
             @else
+                <li class="item_menu_game">
+                    <a href=""><img src="{{ asset('images/button-luan-ban.png') }}" alt=""></a>
+                    <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-luan-ban-active.png') }}" alt=""></a>
+                </li>
                 <li class="item_menu_game">
                     <a href="/dang-nhap"><img src="{{ asset('images/button-dang-nhap.png') }}" alt=""></a>
                 </li>
@@ -72,10 +72,6 @@
                 <a href="/"><img class="main" src="{{ asset('images/button-chinh-dien.png') }}" alt=""></a>
                 <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-chinh-dien-active.png') }}" alt=""></a>
             </li>
-{{--            <li class="item_menu_game">--}}
-{{--                <a href=""><img src="{{ asset('images/button-luan-ban.png') }}" alt=""></a>--}}
-{{--                <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-luan-ban-active.png') }}" alt=""></a>--}}
-{{--            </li>--}}
             @if(session()->get('user'))
                 <li class="item_menu_game">
                     <a href="/tai-khoan"><img src="{{ asset('images/button-tai-khoan.png') }}" alt=""></a>
@@ -85,6 +81,10 @@
                     <a href="/dang-xuat"><img src="{{ asset('images/button-dang-xuat.png') }}" alt=""></a>
                 </li>
             @else
+                <li class="item_menu_game">
+                    <a href=""><img src="{{ asset('images/button-luan-ban.png') }}" alt=""></a>
+                    <a class="active" href="javascript:void(0)"><img class="main" src="{{ asset('images/button-luan-ban-active.png') }}" alt=""></a>
+                </li>
                 <li class="item_menu_game">
                     <a href="/dang-nhap"><img src="{{ asset('images/button-dang-nhap.png') }}" alt=""></a>
                 </li>
