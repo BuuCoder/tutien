0fddef396837a28d395f0cd25f168b25dcb31ab8
Pham Quoc Khai - FullStack (QIT\Admin2) <quockhai@mobcec.com>
2024-07-12 11:23:19 +0700

    update chat real time

--- source/app/Events/MessageSent.php
+++ source/app/Events/MessageSent.php
@@ -4,13 +4,11 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Message;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use InteractsWithSockets, SerializesModels;
 
