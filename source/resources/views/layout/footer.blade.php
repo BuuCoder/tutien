da9ffcfaa519547a200ec3812203a7787c8f0b8d
Majinbuu <majinbuuu113@gmail.com>
2024-07-22 15:42:08 +0700

    update ads

--- source/resources/views/layout/footer.blade.php
+++ source/resources/views/layout/footer.blade.php
@@ -2,34 +2,40 @@
     <img src="{{ asset('images/components/tu-tien-gioi-3.png') }}" alt="">
     <p>Được tạo bởi Majinbuu &copy; Copy right 2024 </p>
     <p>Chúc các bạn tham gia chơi vui vẻ nhé!</p>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
+    <a class="button-ads" href="https://thixevikuk.net/4/7763325">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://geejetag.com/4/7763326">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://ceethipt.com/4/7763909">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://neshigreek.com/4/7763910">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://soatuloaglou.net/4/7763913">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://staisterg.net/4/7763914">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://roastoup.com/4/7763916">
+        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
+    </a>
+    <a class="button-ads" href="https://shooloaree.net/4/7763919">
         <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
     </a>
     <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
         <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
     </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
+    <a class="button-ads" href="https://staisterg.net/4/7763920">
         <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
     </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
+    <a class="button-ads" href="https://choufauphik.net/4/7763923">
         <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
     </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
-        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
-    </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
-        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
-    </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
-        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
-    </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
-        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
-    </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
-        <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
-    </a>
-    <a class="button-ads" href="https://tousupiwaipa.net/4/7757205">
+    <a class="button-ads" href="https://juzaugleed.com/4/7763928">
         <img src="{{ asset('./images/components/button-quang-cao.gif') }}" alt="">
     </a>
 </div>
