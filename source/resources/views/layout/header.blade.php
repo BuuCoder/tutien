9cbceb1ac5ccb3f698a33816eba4c55a3ae3564a
Majinbuu <majinbuuu113@gmail.com>
2024-07-22 15:44:50 +0700

    add monetag

--- source/resources/views/layout/header.blade.php
+++ source/resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{--    <meta name="monetag" content="758854731d2f69594628704f688163b1">--}}
+    <meta name="monetag" content="758854731d2f69594628704f688163b1">
     <title>Tu tiên giới - Majinbuu</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
